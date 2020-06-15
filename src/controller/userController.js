bf627aa9b476bf6ea3f481930e8aa9bd77cf3916
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-15 17:36:09 +0530

    [Tejaswini].refactor some changes userController

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,11 +4,9 @@
 import firebase from 'firebase'
 import jwt from 'jsonwebtoken'
 import serviceConstant from '../const'
-//import Registration from '../component/registration'
 import fire from '../config/fire'
 const db = firebase.firestore();
 export default async function register(data){
-console.log( "data in  in registration",data);
 const user={
 firstName:data.firstName,
 lastName:data.lastName,
@@ -17,15 +15,10 @@ email:data.email,
 password:data.password
 }
 try {
-    console.log("data in usercontroller",user)
 let response=await fire.auth().createUserWithEmailAndPassword(data.email,data.password)
-console.log("this is firebase",response);
 let currentUser=fire.auth().currentUser.uid
-console.log( "current user data in registration",currentUser);
 let userdetails= db.collection('users').doc(currentUser).set(user);
-console.log("register succes",userdetails)
 let verification=fire.auth().currentUser.sendEmailVerification();
-console.log("verification success",verification)
 return response;
 }
 catch (error) {
@@ -38,18 +31,13 @@ return error.message
  * login data form
  */
 export  async function Log(data){
-    //console.log("login enterd into controller",data)
     let user={
     email:data.email,
     password:data.password,
     }
-    //console.log("data is came",userData)
     let response=await fire.auth().signInWithEmailAndPassword(data.email,data.password);
-    console.log("sighned",response)
     let currentUser=fire.auth().currentUser.uid
-    console.log( "current user data",currentUser);
     let userdetails= db.collection('users').doc(currentUser);
-    console.log("login succes",userdetails)
     await userdetails.get().then(function(doc){
         const payload={
           email:serviceConstant.firebaseAuthorization.currentUser.email,
@@ -59,8 +47,6 @@ export  async function Log(data){
           expiresIn:1140
         })
       localStorage.setItem('usertoken',token)
-     // console.log("jwt web token generated",JSON.stringify(userData))
-    
       })
     return response;
     
@@ -75,13 +61,10 @@ let user= {
 email:data.email
 }
 try{
-console.log("enterd in controller");
 let response=await fire.auth().sendPasswordResetEmail(data.email);
-console.log("reset mail send",response);
 return response;
 }
 catch(error){
-console.log(error);
 return error.message
 }
 }
