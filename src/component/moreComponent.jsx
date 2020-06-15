7e67a54a8ef20946b0c5aea7c9c64b97961113ac
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-15 17:17:33 +0530

    [Tejaswini].refactor some changes in editLabel component

--- src/component/moreComponent.jsx
+++ src/component/moreComponent.jsx
@@ -16,14 +16,10 @@
 
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
-import Popper from '@material-ui/core/Popper'
-import IconButton from '@material-ui/core/IconButton'
-import { Paper} from '@material-ui/core';
-import { Button } from '@material-ui/core';
+import { Popper, IconButton, Paper, Button } from '@material-ui/core'
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import {deleteNotes} from '../controller/noteController'
+import { deleteNotes } from '../controller/noteController'
 import Editlabel from '../component/editLable'
-// import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 export class MoreComponent extends Component {
     constructor(props) {
         super(props)
@@ -32,7 +28,7 @@ export class MoreComponent extends Component {
             anchorEl: null,
             open: false,
             delete: false,
-            key:""
+            key: ""
         }
     }
     handlemoremenu = (event) => {
@@ -41,24 +37,19 @@ export class MoreComponent extends Component {
         })
     }
     handleDelete = () => {
-        console.log("data come to delete function",this.props.noteId);
         this.setState({
-        open: false,
-        delete: this.state.delete,
-        key: this.props.noteId
+            open: false,
+            delete: this.state.delete,
+            key: this.props.noteId
         });
-        console.log("response is coming to handle delete",this.props.noteId,this.state.delete)
         let delete1 = {
-        key: this.props.noteId,
-        delete: this.state.delete
+            key: this.props.noteId,
+            delete: this.state.delete
         }
-        console.log("response is coming to handle delete",delete1)
         deleteNotes(delete1).then((res) => {
-        console.log("data is deleted came to in get note component", res)
         })
-        }
+    }
     render() {
-        console.log("key is coming to more component",this.props.noteId)
         const { anchorEl } = this.state;
         const open = Boolean(anchorEl);
         const id = open ? 'simple-popper' : undefined;
@@ -68,14 +59,14 @@ export class MoreComponent extends Component {
                     aria-describedby={id} type="button" onClick={this.handlemoremenu}>
                     <div className="moreverticon" title="more"><MoreVertIcon /></div>
                 </IconButton>
-                <Popper id={id} open={open} anchorEl={anchorEl} style={{zIndex:"9999"}}>
+                <Popper id={id} open={open} anchorEl={anchorEl} style={{ zIndex: "9999" }}>
                     <Paper className="more-paper">
                         <Button id="lablebutton1">
                             <div className="delete" onClick={this.handleDelete}>
                                 Delete note</div>
                         </Button>
-                       <Editlabel
-                       editlabel={this.props.noteId}></Editlabel>
+                        <Editlabel
+                            editlabel={this.props.noteId}></Editlabel>
                         <Button id="lablebutton3">
                             <div className="adddrawing">
                                 Add drawing</div>
