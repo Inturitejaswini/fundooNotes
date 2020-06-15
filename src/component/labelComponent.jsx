7c97ceedc615c5107b7ed867f090de521ebfed8d
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-15 17:12:33 +0530

    [Tejaswini].refactor some changes in editLabel component

--- src/component/labelComponent.jsx
+++ src/component/labelComponent.jsx
@@ -14,28 +14,14 @@
 ******************************************************************************/
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
-import { InputBase } from '@material-ui/core';
-import { Button, IconButton, Card } from '@material-ui/core';
-import { getnotes } from '../controller/noteController'
-import { getlabelscard } from '../controller/noteController'
+import { InputBase, Button, IconButton, Card, Dialog } from '@material-ui/core';
+import { getnotes, getlabelscard, noteUpdate, ArchiveNotes, updatePin, updateunPin, deleteNotes } from '../controller/noteController'
 import image1 from '../assets/pushpin.jpeg'
-import CropOriginalIcon from '@material-ui/icons/CropOriginal';
+import { CropOriginalIcon, PersonAddIcon, ColorLensIcon, ArchiveOutlinedIcon, RedoIcon, UndoIcon, } from '@material-ui/icons';
 import AppBar1 from '../component/appBar'
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import ColorLensIcon from '@material-ui/icons/ColorLens';
-import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
-import RedoIcon from '@material-ui/icons/Redo';
-import UndoIcon from '@material-ui/icons/Undo'
 import MoreComponent from '../component/moreComponent';
-import { noteUpdate } from '../controller/noteController'
-import { ArchiveNotes } from '../controller/noteController'
-import { deleteNotes } from '../controller/noteController'
-import Dialog from '@material-ui/core/Dialog';
 import Reminder from '../component/reminder'
 import Color from '../component/colorComponent'
-import { updatePin } from '../controller/noteController'
-import { updateunPin } from '../controller/noteController'
-import ScheduleIcon from '@material-ui/icons/Schedule';
 class Labelcomponent extends Component {
     constructor(props) {
         super(props)
@@ -52,24 +38,19 @@ class Labelcomponent extends Component {
         this.setState({
             title: event.target.value,
         });
-        // console.log("taking title", this.state.title);
     }
     handlechangetakeNote = (event) => {
         this.setState({ takeNote: event.target.value });
-        // console.log("taking notes",this.state.takeNote);
     }
     handleNote = async (title, takeNote, key) => {
-        //console.log("id in get",key);
         await this.setState({
             title: title,
             open: !this.state.open,
             takeNote: takeNote,
             key: key
         })
-        // console.log("taking title", this.state.title,this.state.takeNote,this.state.key);
     }
     handleDelete = () => {
-        console.log("data come to delete function");
         this.setState({
             open: false,
             delete: this.state.delete,
@@ -77,64 +58,50 @@ class Labelcomponent extends Component {
             takeNote: this.state.takeNote,
             key: this.state.key
         });
-        //console.log("response is coming to handle delete",this.state.key,this.state.delete)
         let delete1 = {
             title: this.state.title,
             takeNote: this.state.takeNote,
             key: this.state.key,
             delete: this.state.delete
         }
-        //console.log("response is coming to handle delete",delete1)
         deleteNotes(delete1).then((res) => {
-            //console.log("data is deleted came to in get note component", res)
         })
     }
     handlePin = () => {
-        console.log("gggggg--->");
         this.setState({
-            // selectedValue: value, 
             open: false,
             pin: this.state.pin,
             title: this.state.title,
             takeNote: this.state.takeNote,
             key: this.state.key
         });
-        // console.log("response is coming to handle pin",this.state.pin)
         let pin = {
             title: this.state.title,
             pin: this.state.pin,
             takeNote: this.state.takeNote,
             key: this.state.key
         }
-        //console.log("response is coming to handle pin", pin.pin)
         updatePin(pin).then((res) => {
-            // console.log("data is updated came to in get note component", res)
         })
     }
     changehandleunPin = (key, title, takeNote) => {
-        console.log("gggggg--->changing");
         this.setState({
-            // selectedValue: value, 
             open: false,
             pin: this.state.pin,
             title: title,
             takeNote: takeNote,
             key: key
         });
-        console.log("response is coming to handle upade", this.state.pin)
         let pin = {
             title: title,
             pin: !this.state.pin,
             takeNote: takeNote,
             key: key
         }
-        console.log("response is coming to handle pin upadation", pin.pin)
         updateunPin(pin).then((res) => {
-            console.log("data is updated came to in get note component", res)
         })
     }
     handleArchive = (title, takeNote, key) => {
-        console.log("data come to archive function", title, takeNote, key);
         this.setState({
             open: false,
             delete: this.state.delete,
@@ -143,7 +110,6 @@ class Labelcomponent extends Component {
             key: key,
             archive: this.state.archive
         });
-        console.log("response is coming to handle archive", this.state.key, this.state.archive)
         let archive1 = {
             title: title,
             takeNote: takeNote,
@@ -151,29 +117,22 @@ class Labelcomponent extends Component {
             delete: this.state.delete,
             archive: this.state.archive
         }
-        console.log("response is coming to handle delete", archive1.title, archive1.key, archive1.archive)
         ArchiveNotes(archive1).then((res) => {
-            console.log("data is archived came to in get note component")
         })
     }
     handleUpdate = () => {
-        // console.log("gggggg--->",value);
         this.setState({
-            // selectedValue: value, 
             open: false,
             title: this.state.title,
             takeNote: this.state.takeNote,
             key: this.state.key
         });
-        // console.log("response is coming to handle upade",this.state.key)
         let update = {
             title: this.state.title,
             takeNote: this.state.takeNote,
             key: this.state.key
         }
-        //console.log("response is coming to handle upade", update)
         noteUpdate(update).then((res) => {
-            // console.log("data is updated came to in get note component", res)
         })
     };
     componentDidMount = () => {
@@ -186,7 +145,6 @@ class Labelcomponent extends Component {
                 this.setState({
                     noteArray: res
                 })
-                console.log("noteData", this.state.noteArray)
                 this.state.noteArray.map((key) => {
                 })
             })
@@ -197,11 +155,9 @@ class Labelcomponent extends Component {
     getAllLables = () => {
         getlabelscard()
             .then(res => {
-                console.log("dataaaaaaa for labels", res);
                 this.setState({
                     noteArray1: res
                 })
-                console.log("noteData", this.state.noteArray1)
             })
 
     }
@@ -216,15 +172,10 @@ class Labelcomponent extends Component {
         })
     }
     handleAddLabel = (event) => {
-        console.log("in event");
     }
     render() {
-        // const keyidlabel=this.props.location.state
-        // console.log("key is coming to label component",this.props,keyidlabel)
         var pinData = this.state.noteArray1.map(key1 => {
-            console.log("to get labels in get note component", key1.data().label, key1.data().labelkey, key1.id)
             if (this.props.location.state == key1.data().label) {
-                //  console.log("to get labels in get note component", key1.data().label, keyid)
                 return (
                     <div className="getting_labels_inDrawercomponent_inlabelComponent">
                         {this.state.noteArray.map(key => {
@@ -246,7 +197,6 @@ class Labelcomponent extends Component {
                                                     </div>
                                                     {(key.data().remainder) != null ? (
                                                         <div className="date_and_time">
-                                                            {/* <ScheduleIcon></ScheduleIcon> */}
                                                             <InputBase id="inner_date_and_time" value={key.data().remainder}></InputBase></div>)
                                                         :
                                                         (
@@ -265,7 +215,6 @@ class Labelcomponent extends Component {
                                                         })}</div>
                                                 </div>
                                                 <div className="note-card-div-label">
-
                                                     <Reminder
                                                         noteId={key.id}></Reminder>
                                                     <IconButton className="personadd">
@@ -294,7 +243,6 @@ class Labelcomponent extends Component {
                                         </div>
                                         <Dialog aria-labelledby="simple-dialog-title" open={this.state.open}>
                                             <div className="card_getNote">
-
                                                 <div id="getNotes-align">
                                                     <InputBase placeholder="title" value={this.state.title} onChange={this.handlechangeTitle}>
                                                     </InputBase> <IconButton className="gettnote_img_img">
@@ -336,7 +284,7 @@ class Labelcomponent extends Component {
                                                     </IconButton>
                                                     <IconButton className="close1" onClick={this.handleUpdate}>
                                                         close
-                              </IconButton>
+                                                   </IconButton>
                                                 </div>
                                             </div>
                                         </Dialog>
