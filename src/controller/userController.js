0573d07a4a53a470fef558c8f331f415954845e8
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 21:37:51 +0530

    [tejaswini]changed some code in  usercontroller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,10 @@
 /**
  * registration form.
  */
-import firebase from 'firebase'
-import jwt from 'jsonwebtoken'
-import serviceConstant from '../const'
-import fire from '../config/fire'
+import firebase from "firebase";
+import jwt from "jsonwebtoken";
+import serviceConstant from "../const";
+import fire from "../config/fire";
 const db = firebase.firestore();
 export default async function register(data) {
   const user = {
@@ -12,17 +12,18 @@ export default async function register(data) {
     lastName: data.lastName,
     fullName: data.fullName,
     email: data.email,
-    password: data.password
-  }
+    password: data.password,
+  };
   try {
-    let response = await fire.auth().createUserWithEmailAndPassword(data.email, data.password)
-    let currentUser = fire.auth().currentUser.uid
-    let userdetails = db.collection('users').doc(currentUser).set(user);
+    let response = await fire
+      .auth()
+      .createUserWithEmailAndPassword(data.email, data.password);
+    let currentUser = fire.auth().currentUser.uid;
+    let userdetails = db.collection("users").doc(currentUser).set(user);
     let verification = fire.auth().currentUser.sendEmailVerification();
     return response;
-  }
-  catch (error) {
-    return error.message
+  } catch (error) {
+    return error.message;
   }
 }
 /**
@@ -33,37 +34,41 @@ export async function Log(data) {
   let user = {
     email: data.email,
     password: data.password,
-  }
-  let response = await fire.auth().signInWithEmailAndPassword(data.email, data.password);
-  let currentUser = fire.auth().currentUser.uid
-  let userdetails = db.collection('users').doc(currentUser);
+  };
+  let response = await fire
+    .auth()
+    .signInWithEmailAndPassword(data.email, data.password);
+  let currentUser = fire.auth().currentUser.uid;
+  let userdetails = db.collection("users").doc(currentUser);
   await userdetails.get().then(function (doc) {
     const payload = {
       email: serviceConstant.firebaseAuthorization.currentUser.email,
       password: serviceConstant.firebaseAuthorization.currentUser.password,
-    }
-    let token = jwt.sign(payload, serviceConstant.firebaseAuthorization.currentUser.uid, {
-      expiresIn: 1140
-    })
-    localStorage.setItem('usertoken', token)
-  })
+    };
+    let token = jwt.sign(
+      payload,
+      serviceConstant.firebaseAuthorization.currentUser.uid,
+      {
+        expiresIn: 1140,
+      }
+    );
+    localStorage.setItem("usertoken", token);
+  });
   return response;
-
 }
 /**
- * 
+ *
  * @param {data} data as a perameter
  */
 
 export async function forgot(data) {
   let user = {
-    email: data.email
-  }
+    email: data.email,
+  };
   try {
     let response = await fire.auth().sendPasswordResetEmail(data.email);
     return response;
-  }
-  catch (error) {
-    return error.message
+  } catch (error) {
+    return error.message;
   }
 }
