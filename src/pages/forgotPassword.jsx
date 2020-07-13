073200c60b6d80edc553fa7aedc4d1157b600dc2
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 21:39:55 +0530

    [tejaswini] remove file comments and rename clasnames in forgetPassword page

--- src/pages/forgotPassword.jsx
+++ src/pages/forgotPassword.jsx
@@ -1,66 +1,49 @@
-
-/******************************************************************************
- *  Execution       :   1. default node         cmd> node forgotPassword.jsx 
- *                      2. if nodemon installed cmd> nodemodule forgotPassword.jsx
- * 
- *  Purpose         : creating forgotPassword form.
- *  @description    
- * 
- *  @file           :forgotPassword.jsx
- *  @overview       :forgotPassword form problem.
- *  @module         :forgotPassword - This is optional if expeclictly its an npm or local package
- *  @author         :Tejaswini<chowdarytejaswini2@gmail.com>
- *  @version        :1.0
- *  @since          :9-1-2019
- ******************************************************************************/
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom';
-import { Card, TextField, Button } from '@material-ui/core'
-import forgot from '../controller/userController'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { Card, TextField, Button } from "@material-ui/core";
+import forgot from "../controller/userController";
 export class ForgotPassword extends Component {
-    constructor() {
-        super()
-        this.state = {
-            email: this.email
-        }
-    }
-    handleChangeEmail = (event) => {
-        this.setState({ email: event.target.value });
-    }
-    handleClick = () => {
-        const user = {
-            email: this.state.email
-        }
-        forgot(user).then(() => {
-        })
-    }
+  constructor() {
+    super();
+    this.state = {
+      email: this.email,
+    };
+  }
+  handleChangeEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+  handleClick = () => {
+    const user = {
+      email: this.state.email,
+    };
+    forgot(user);
+  };
 
-    render() {
-        return (
-            <div className="login_container-forgot">
-                <Card className="forgot_card-card" style={{backgroundColor:"pink"}}>
-                    <div className="abcd">
-                        <h3>fundooapp</h3>
-                    </div>
-                    <div className="efgh">
-                     <TextField
-                        name="email"
-                        value={this.state.email}
-                        label="email"
-                        variant="outlined"
-                        onChange={this.handleChangeEmail}  />
-                    </div>
-                        <div className="hfgh" >
-                            <Button onClick={this.handleClick} >
-                                <div className="sub">                 
-                                    submit 
-                                 </div>                            
-                            </Button>
-                        </div>
-                 </Card>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login_container-forgot">
+        <Card className="forgot_card-card">
+          <div className="abcd">
+            <h3>fundooapp</h3>
+          </div>
+          <div className="email-div">
+            <TextField
+              name="email"
+              value={this.state.email}
+              label="email"
+              variant="outlined"
+              onChange={this.handleChangeEmail}
+            />
+          </div>
+          <div className="submit-div">
+            <Button onClick={this.handleClick}>
+              <div className="sub">submit</div>
+            </Button>
+          </div>
+        </Card>
+      </div>
+    );
+  }
 }
 
-export default withRouter(ForgotPassword)
+export default withRouter(ForgotPassword);
