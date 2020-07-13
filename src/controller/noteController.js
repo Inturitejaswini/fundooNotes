69215605ca8ca804f0aa70dffb928356e2c9eb0d
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 21:37:14 +0530

    [tejaswini]changed some code in  notecontroller

--- src/controller/noteController.js
+++ src/controller/noteController.js
@@ -1,13 +1,12 @@
 /**
  * @param {notes} data as a perameter
  */
-import firebase from 'firebase'
-import serviceConstant from '../const'
-import fire from '../config/fire'
-import jwt_decode from 'jwt-decode'
+import firebase from "firebase";
+import serviceConstant from "../const";
+import fire from "../config/fire";
+import jwt_decode from "jwt-decode";
 const db = firebase.firestore();
 export async function notes(data) {
-
   let userData = {
     title: data.title,
     takeNote: data.takeNote,
@@ -16,58 +15,63 @@ export async function notes(data) {
     pin: data.pin,
     key: data.key,
     remainder: data.remainder,
-    note: data.note
-  }
-  let response = await db.collection("notes").add(userData)
+    note: data.note,
+  };
+  let response = await db.collection("notes").add(userData);
   return response;
 }
 
 /**
- * get notes 
+ * get notes
  */
 export async function getNotes() {
   try {
-    let note = []
-    let response = await db.collection("notes").get()
-      .then(res => {
-        res.forEach(data => {
-          note.push(data)
+    let note = [];
+    let response = await db
+      .collection("notes")
+      .get()
+      .then((res) => {
+        res.forEach((data) => {
+          note.push(data);
         });
-      })
+      });
     return note;
-  }
-  catch (error) {
-    return error.message
+  } catch (error) {
+    return error.message;
   }
 }
 /**
- * 
+ *
  * @param {data} data  as a perameter
  */
 export async function noteUpdate(data) {
   let userData = {
     title: data.title,
     takeNote: data.takeNote,
-    key: data.key
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return (response)
+    key: data.key,
+  };
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
-
 
 export async function deleteNotes(data) {
   let userData = {
     key: data.key,
-    delete: data.delete
-  }
+    delete: data.delete,
+  };
   if (userData.delete == false) {
-    userData.delete = true
+    userData.delete = true;
+  } else {
+    userData.delete = false;
   }
-  else {
-    userData.delete = false
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(userData.key).update(userData);
-  return (response)
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(userData.key)
+    .update(userData);
+  return response;
 }
 
 export async function ArchiveNotes(data) {
@@ -76,58 +80,63 @@ export async function ArchiveNotes(data) {
     takeNote: data.takeNote,
     key: data.key,
     archive: data.archive,
-  }
+  };
   if (userData.archive == false) {
-    userData.archive = true
+    userData.archive = true;
+  } else {
+    userData.archive = false;
   }
-  else {
-    userData.archive = false
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return (response)
-
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 export async function ArchiveNotesCreate(data) {
-  const token = localStorage.usertoken
-  const decodedJwt = jwt_decode(token)
+  const token = localStorage.usertoken;
+  const decodedJwt = jwt_decode(token);
   let currentuser = fire.auth().currentUser.email;
-  let emailid = localStorage.setItem('notes', currentuser);
+  let emailid = localStorage.setItem("notes", currentuser);
   let userData = {
     title: data.title,
     takeNote: data.takeNote,
     email: currentuser,
     archive: data.archive,
-    token_id: token
-  }
+    token_id: token,
+  };
   if (userData.archive == false) {
-    userData.archive = true
+    userData.archive = true;
+  } else {
+    userData.archive = false;
   }
-  else {
-    userData.archive = false
-  }
-  let response = await db.collection("notes").add(userData)
+  let response = await db.collection("notes").add(userData);
   return response;
 }
 export async function unArchiveNotes(data) {
   let userData = {
     key: data.key,
     archive: data.archive,
-  }
+  };
   if (userData.archive == false) {
-    userData.archive = true
+    userData.archive = true;
+  } else {
+    userData.archive = false;
   }
-  else {
-    userData.archive = false
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return (response)
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 
 export async function deleteNotesPermenently(data) {
   let userData = {
     key: data.key,
-  }
-  let response1 = await serviceConstant.firestore.collection('notes').doc(data.key).delete();
+  };
+  let response1 = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .delete();
   return response1;
 }
 
@@ -136,34 +145,37 @@ export async function restoreNotes(data) {
     title: data.title,
     takeNote: data.takeNote,
     key: data.key,
-    delete: data.delete
-  }
+    delete: data.delete,
+  };
   if (userData.delete == false) {
-    userData.delete = true
+    userData.delete = true;
+  } else {
+    userData.delete = false;
   }
-  else {
-    userData.delete = false
-  }
-  let response1 = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
+  let response1 = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
   return response1;
 }
-
 
 export async function updatePin(data) {
   let userData = {
     title: data.title,
     takeNote: data.takeNote,
     key: data.key,
-    pin: data.pin
-  }
+    pin: data.pin,
+  };
   if (userData.pin == false) {
-    userData.pin = true
+    userData.pin = true;
+  } else {
+    userData.pin = false;
   }
-  else {
-    userData.pin = false
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return (response)
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 
 export async function updateUnPin(data) {
@@ -171,113 +183,127 @@ export async function updateUnPin(data) {
     title: data.title,
     takeNote: data.takeNote,
     key: data.key,
-    pin: data.pin
-  }
+    pin: data.pin,
+  };
   if (userData.pin == false) {
-    userData.pin = true
+    userData.pin = true;
+  } else {
+    userData.pin = false;
   }
-  else {
-    userData.pin = false
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return response
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 
 export async function remainder(data) {
   let userData = {
     remainder: data.timeDate,
     key: data.key,
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(userData);
-  return response
+  };
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 
 export async function checkBox(data) {
   let userData = {
     checkBox: data.checkBox,
-    labelkey: data.labelkey
-  }
+    labelkey: data.labelkey,
+  };
   if (userData.checkBox == false) {
-    userData.checkBox = true
+    userData.checkBox = true;
+  } else {
+    userData.checkBox = false;
   }
-  else {
-    userData.checkBox = false
-  }
-  let response = await serviceConstant.firestore.collection("labels").doc(data.labelkey).update(userData);
-  return response
+  let response = await serviceConstant.firestore
+    .collection("labels")
+    .doc(data.labelkey)
+    .update(userData);
+  return response;
 }
-
 
 export async function getLabelsCard() {
   try {
-    const token = localStorage.usertoken
-    const decodedJwt = jwt_decode(token)
+    const token = localStorage.usertoken;
+    const decodedJwt = jwt_decode(token);
     var labels = [];
-    await serviceConstant.firestore.collection("labels").where("email", "==", decodedJwt.email).get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        labels.push(doc)
+    await serviceConstant.firestore
+      .collection("labels")
+      .where("email", "==", decodedJwt.email)
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          labels.push(doc);
+        });
       });
-    })
-    return labels
-  }
-  catch (error) {
-    return error.message
+    return labels;
+  } catch (error) {
+    return error.message;
   }
 }
 
-
 export async function createLabelNotes(data) {
-  const token = localStorage.usertoken
-  const decodedJwt = jwt_decode(token)
+  const token = localStorage.usertoken;
+  const decodedJwt = jwt_decode(token);
   let currentuser = fire.auth().currentUser.email;
-  let emailid = localStorage.setItem('notes', currentuser);
+  let emailid = localStorage.setItem("notes", currentuser);
   let userData = {
     label: data.takeNote,
     email: currentuser,
     key: data.key,
-    checkBox: data.checkBox
-  }
-  let response = await db.collection("labels").add(userData)
+    checkBox: data.checkBox,
+  };
+  let response = await db.collection("labels").add(userData);
   let userdata1 = {
-    labelkey: response.id
-  }
+    labelkey: response.id,
+  };
   return response;
 }
 
 export async function getLabels(data) {
   try {
-
     let userData = {
-      key: data.key
-    }
+      key: data.key,
+    };
     var labels = [];
-    await serviceConstant.firestore.collection("labels").where("key", "==", userData.key).get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        labels.push(doc)
+    await serviceConstant.firestore
+      .collection("labels")
+      .where("key", "==", userData.key)
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          labels.push(doc);
+        });
       });
-    })
-    return labels
-  }
-  catch (error) {
-    return error.message
+    return labels;
+  } catch (error) {
+    return error.message;
   }
 }
 export async function updateLabel(data) {
   let userData = {
     key: data.key,
-    label: data.label
-  }
-  let response = await serviceConstant.firestore.collection('labels').doc(data.key).update(userData);
-  return (response)
+    label: data.label,
+  };
+  let response = await serviceConstant.firestore
+    .collection("labels")
+    .doc(data.key)
+    .update(userData);
+  return response;
 }
 
 export async function UpdateColors(data) {
   let noteColor = {
     key: data.key,
-    color: data.color
-  }
-  let response = await serviceConstant.firestore.collection('notes').doc(data.key).update(noteColor);
-  return (response)
+    color: data.color,
+  };
+  let response = await serviceConstant.firestore
+    .collection("notes")
+    .doc(data.key)
+    .update(noteColor);
+  return response;
 }
-
-
