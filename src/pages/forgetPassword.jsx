5cb1e426dc72cfd7f98d69cc70f9d8598bbc4e71
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-18 17:13:05 +0530

    [tejaswini] refactor forgetPassword component with validation

--- src/pages/forgetPassword.jsx
+++ src/pages/forgetPassword.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import { Card, TextField, Button } from "@material-ui/core";
+import { Card, TextField, Button,Snackbar} from "@material-ui/core";
 import {forget} from "../controller/userController";
 export class ForgetPassword extends Component {
   constructor() {
     super();
     this.state = {
       email: this.email,
+      snackbarOpen: false,
+      snackbarMessage: "",
     };
   }
   handleChangeEmail = (event) => {
-    this.setState({ email: event.target.value });
+    event.target.value.match("^([a-zA-Z0-9]+@+[a-zA-Z0-9]+.+[A-z])*$") != null
+      ? this.setState({ email: event.target.value })
+      : this.setState({
+          snackbarOpen: true,
+          snackbarMessage: " *enter valid email",
+        });
   };
   handleClick = () => {
     const user = {
@@ -22,6 +29,18 @@ export class ForgetPassword extends Component {
   render() {
     return (
       <div className="login_container-forgot">
+        <Snackbar
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+              open={this.state.snackbarOpen}
+              autoHideDuration={6000}
+              onClose={this.snackbarOpen}
+              message={
+                <span id="messege-id">{this.state.snackbarMessage}</span>
+              }
+            ></Snackbar>
         <Card className="forgot_card-card">
           <div className="abcd">
             <h3>fundooapp</h3>
